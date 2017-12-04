dca04651f95f19d457b218dd8273d18baf44c10a
Devin Haslam <dhasl002odu.edu>
2017-12-04 16:02:16 -0500

    most of project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ClassProject-Bioinformatics-ColorScript
 Given a protein and a connection map, the script will color amino acids that are indicated as matching by the contact map
+
+To use, First open Chimera.
+
+Open the protein that you would like to see the contacts.
+Download the contact map and delete everything in the file except the contacts.
+Replace all spaces with commas in the contact map file.
+
+To run in Chimera:
+there are 4 parameters, the location of the script, the location of the contact map, whether the script should recolor 
+residues (0 for no 1 for yes), the lowest confidence interval the script should use.
+
+EX: run C:\Users\dhaslam\Desktop\contactColorer.py "C:/Users/dhaslam/Downloads/274635.all_in_one/274635.all_in_one/274635.contactmap.txt" 0 .8
---
+++ contactColorer.py
@@ -0,0 +1,39 @@
+import Midas
+import sys
+colors = ["tan", "sienna", "brown", "salmon", "red", "sandy brown", "orange red", "orange", "goldenrod", "gold", "yellow", "khaki", "dark khaki", "dark olive green", "olive drab", "chartreuse", "green", "dark green", "dark cyan", "light sea green", "aquamarine", "cyan", "deep sky blue", "steel blue", "sky blue", "blue", "medium blue", "navy blue", "medium purple", "purple", "plum", "orchid", "magenta", "dark magenta", "hot pink", "deep pink", "slate gray", "dark slate gray", "white"]
+#39 colors
+
+AAList = []
+
+#path = "C:/Users/dhaslam/Downloads/274635.all_in_one/274635.all_in_one/274635.contactmap.txt"
+path = sys.argv[1]
+
+it = 0
+i = 0
+breakVar = 0
+noRepeat = int(sys.argv[2])
+
+with open(path) as inf:
+  for line in inf:
+    AA1, AA2, trash1, trash2, trash3 = line.strip().split(",")
+    Midas.color(colors[it], sel=':' + str(AA1))
+    Midas.color(colors[it], sel=':' + str(AA2))
+    it = it + 1
+    next(inf)
+    
+    if it == 38:
+      break
+    
+    if float(trash3) < float(sys.argv[3]):
+      break
+    
+    if noRepeat == 1:
+      for i in range (0,len(AAList)):
+        if AA1 == AAList[i] or AA2 == AAList[i]:
+          breakVar = 1
+      
+      if breakVar == 1:
+        break
+    
+    AAList.append(AA1)
+    AAList.append(AA2)
